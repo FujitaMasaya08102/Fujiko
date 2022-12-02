cba9976e9558c55f05e097045feee7487d081aa0
藤田雅弥 <21010027fm@edu.fca.ac.jp>
2022-12-02 16:19:54 +0900

    a

--- MyNewGame/Assets/scripts/StageGenerator.cs
+++ MyNewGame/Assets/scripts/StageGenerator.cs
@@ -37,12 +37,7 @@ public class StageGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // プレハブの位置をランダムで設定
-        float x = Random.Range(-5.0f, 5.0f);
-        float z = Random.Range(-5.0f, 5.0f);
-        Vector3 pos = new Vector3(x, 10.0f, z);
-        // プレハブを生成
-        Instantiate(StagePrefab, pos, Quaternion.identity);
+
 
         int targetPosIndex = (int)(Target.position.z / StageSize);
 
